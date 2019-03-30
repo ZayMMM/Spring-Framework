85db1a5b49ba4c60c2fcd76cfa9fe0af8466932b
ZayMaungMaungMyint <zaymaungmaungmyint.info@gmail.com>
2019-03-30 13:00:05 +0630

    update project name

---
+++ spring-security-demo-03-custom-login-form/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configurable
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// add our user for in memory authentication
+		UserBuilder user = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+			.withUser(user.username("john").password("test123").roles("EMPLOYEE"))
+			.withUser(user.username("mary").password("test123").roles("MANAGER"))
+			.withUser(user.username("susan").password("test123").roles("ADMIN"));
+			
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()
+				.loginPage("/showMyLoginPage")
+				.loginProcessingUrl("/authenticateTheUser")
+				.permitAll();
+	}
+	
+	
+
+	
+}
